ed2513f6cc3db73c9bcbe6fb0db3c6aa07b3154c
Jordan Christiansen <jordan.christiansen@target.com>
2021-02-05 17:17:48 -0600

    Foramt with K&R style

--- getaddrinfo.c
+++ getaddrinfo.c
@@ -8,16 +8,14 @@
 #define EX_NOHOST 68
 #define EX_SOFTWARE 70
 
-void usage() {
-	fputs(
-			"usage: getaddrinfo hostname\n"
-			"\n"
-			"getaddrinfo is a tool for doing DNS lookups. It uses the POSIX\n"
-			"standard getaddrinfo(3) interface. Give a hostname as an argument\n"
-			"and host will respond with the IP addresses it maps to.\n",
-
-			stderr
-		 );
+void usage()
+{
+	fputs("usage: getaddrinfo hostname\n"
+	      "\n"
+	      "getaddrinfo is a tool for doing DNS lookups. It uses the POSIX\n"
+	      "standard getaddrinfo(3) interface. Give a hostname as an argument\n"
+	      "and host will respond with the IP addresses it maps to.\n",
+	      stderr);
 }
 
 int main(int argc, char *argv[])
@@ -30,28 +28,26 @@ int main(int argc, char *argv[])
 	struct addrinfo *addrs;
 	int err = getaddrinfo(argv[1], NULL, NULL, &addrs);
 	switch (err) {
-		case 0:
-			break;
-		case EAI_NONAME:
-			fprintf(stderr, "Host not found: %s\n", argv[1]);
-			return EX_NOHOST;
-			break;
-		default:
-			puts(gai_strerror(err));
-			return EX_SOFTWARE;
+	case 0:
+		break;
+	case EAI_NONAME:
+		fprintf(stderr, "Host not found: %s\n", argv[1]);
+		return EX_NOHOST;
+		break;
+	default:
+		puts(gai_strerror(err));
+		return EX_SOFTWARE;
 	}
 
-	for (struct addrinfo *addr = addrs; addr != NULL; addr = addr->ai_next) {
+	for (struct addrinfo * addr = addrs; addr != NULL; addr = addr->ai_next) {
 		struct sockaddr *sa = addr->ai_addr;
 		socklen_t sa_len = addr->ai_addrlen;
 		char ip_address[128] = "";
 
-		int err = getnameinfo(
-				sa, sa_len,
-				ip_address, sizeof ip_address,
-				NULL, 0,
-				NI_NUMERICHOST
-				);
+		int err = getnameinfo(sa, sa_len,
+				      ip_address, sizeof ip_address,
+				      NULL, 0,
+				      NI_NUMERICHOST);
 		if (err != 0) {
 			puts(gai_strerror(err));
 			return EX_SOFTWARE;
